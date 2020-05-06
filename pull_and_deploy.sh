9aa9cefdb2d336297a5c03d5599b3ef665aee90f
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-05-06 17:07:06 +0200

    pull and deploy

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cd /home/kombos/intranet.aio.eu/aioportal
+cd /home/kombos/proditec/aioportal
 RESPULL=`git pull`
 if [[ $RESPULL != *"Already up to date"* ]]; then
   echo "===>Rebuilding HUGO static file"
